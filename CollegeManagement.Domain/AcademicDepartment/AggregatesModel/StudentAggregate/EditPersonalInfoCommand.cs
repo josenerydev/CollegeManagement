d5e41ac0446458a7b74e7eaef865a45b4dff1292
josenerydev <josenerydev@gmail.com>
2019-12-20 15:48:30 -0300

    Add EditPersonalInfoCommand

---
+++ CollegeManagement.Domain/AcademicDepartment/AggregatesModel/StudentAggregate/EditPersonalInfoCommand.cs
@@ -0,0 +1,45 @@
+﻿using CollegeManagement.Domain.SeedWork;
+using CSharpFunctionalExtensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollegeManagement.Domain.AcademicDepartment.AggregatesModel.StudentAggregate
+{
+    public sealed class EditPersonalInfoCommand : ICommand
+    {
+        public long Id { get; }
+        public string Name { get; }
+        public string Email { get; }
+
+        public EditPersonalInfoCommand(long id, string name, string email)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+        }
+
+        internal sealed class EditPersonalInfoCommandHandler : ICommandHandler<EditPersonalInfoCommand>
+        {
+            private readonly SessionFactory _sessionFactory;
+
+            public EditPersonalInfoCommandHandler(SessionFactory sessionFactory)
+            {
+                _sessionFactory = sessionFactory;
+            }
+
+            public Result Handle(EditPersonalInfoCommand command)
+            {
+                var unitOfWork = new UnitOfWork(_sessionFactory);
+                var studentRepository = new StudentRepository(unitOfWork);
+                Student student = studentRepository.GetById(command.Id);
+                if (student == null)
+                    return Result.Failure($"No student found for Id {command.Id}");
+
+                unitOfWork.Commit();
+
+                return Result.Ok();
+            }
+        }
+    }
+}
